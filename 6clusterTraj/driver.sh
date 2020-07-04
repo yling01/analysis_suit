2792d31aaf4ffbc9c872bf53e35e7d36d6749e6a
Yingjie Ling <yling01@pcomp47.lux.tufts.edu>
2020-07-03 22:04:51 -0400

    update

--- 6clusterTraj/driver.sh
+++ 6clusterTraj/driver.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-echo "What is the directory of this dPCA process"
-read DIR
 for i in {1..2}
 do
-    cd ${DIR}/7clusterTraj/s${i}
+    cd s${i}
     ./Sh_make_cluster_xtc.sh
+    cd ../
 done
 
