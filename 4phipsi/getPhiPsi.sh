f82d8505f9667026f3b0b0ad38e5e8fb042b16ed
Yingjie Ling <yling01@pcomp47.lux.tufts.edu>
2020-07-03 21:54:35 -0400

    update

--- 4phipsi/getPhiPsi.sh
+++ 4phipsi/getPhiPsi.sh
@@ -3,7 +3,7 @@ xtc=../3*/all.trr
 gro=../2*/dpca.gro
 ndx=../2*/dpca.ndx
 
-vec=eigenvec.trr 
+vec=eigenvec.trr
 DNAME=pc1_pc2_pc3
 
 gmx_mpi covar -n $ndx -f $xtc -s $gro -ascii -xpm -xpma -nofit -nomwa -noref -nopbc &> covar.log
@@ -24,13 +24,13 @@ function project_all () {
   gmx_mpi anaeig -s $gro -n $ndx -v $vec -f $xtc -2d $out1 -first 1 -last 2 &> anaeig_all1.log
   gmx_mpi anaeig -s $gro -n $ndx -v $vec -f $xtc -2d $out2 -first 1 -last 3 &> anaeig_all2.log
 
-  xtc=../3*/s1/s1${PROT}_all.trr 
+  xtc=../3*/s1/s1${PROT}_all.trr
   out1=$dname/s1${PROT}_1.xvg
   out2=$dname/s1${PROT}_2.xvg
   gmx_mpi anaeig -s $gro -n $ndx -v $vec -f $xtc -2d $out1 -first 1 -last 2 &> anaeig_s11.log
   gmx_mpi anaeig -s $gro -n $ndx -v $vec -f $xtc -2d $out2 -first 1 -last 3 &> anaeig_s12.log
 
-  xtc=../3*/s2/s2${PROT}_all.trr 
+  xtc=../3*/s2/s2${PROT}_all.trr
   out1=$dname/s2${PROT}_1.xvg
   out2=$dname/s2${PROT}_2.xvg
   gmx_mpi anaeig -s $gro -n $ndx -v $vec -f $xtc -2d $out1 -first 1 -last 2 &> anaeig_s21.log
@@ -47,7 +47,7 @@ function combine(){
    inp1=pc1_pc2_pc3/${fname}_1.xvg
    inp2=pc1_pc2_pc3/${fname}_2.xvg
 
-   tail -n +18 $inp1 > tmp1 
+   tail -n +18 $inp1 > tmp1
    tail -n +18 $inp2 > tmp2
 
    echo "   Processing $inp1 $inp2 ..."
@@ -59,6 +59,3 @@ combine s1${PROT}
 combine s2${PROT}
 
 rm tmp*
-
-cp pc1_pc2_pc3/s1*.txt ../5*
-cp pc1_pc2_pc3/s2*.txt ../5*
