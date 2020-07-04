5fa9b7c68a609eda2262d06041fe6c1797003ab3
Yingjie Ling <yling01@pcomp50.lux.tufts.edu>
2020-07-03 21:38:47 -0400

    update

--- 1trimTrajectory/driver.sh
+++ 1trimTrajectory/driver.sh
@@ -13,13 +13,14 @@ read start
 echo "Enter end time in ps: "
 read end
 
-sed -i s.STARTTOCHANGE.${start} trim.sh
-sed -i s.ENDTOCHANGE.${end} trim.sh
+sed -i s.STARTTOCHANGE.${start}.g trim.sh
+sed -i s.ENDTOCHANGE.${end}.g trim.sh
 sed -i s.DIRTOCHANGE.${dir}.g trim.sh
 sed -i s.LENGTHTOCHANGE.${seqLength}.g trim.sh
 sed -i s.SEQUENCETOCHANGE.${seq}.g trim.sh
 
 echo "Getting backbone atoms ..."
+cp ${dir}/9*/npt.gro curr.gro
 python writeIndices.py --Omega True --gro curr.gro
 
 sbatch submit.job
