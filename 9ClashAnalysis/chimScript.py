e0b47ca8965b03579b7f45a70c91ef60ec6fcebd
yling01 <yingjie.ling@tufts.edu>
2020-07-05 17:15:40 -0400

    update

--- 9ClashAnalysis/chimScript.py
+++ 9ClashAnalysis/chimScript.py
@@ -1,4 +1,4 @@
 from chimera import runCommand
 runCommand('open Skp2_10A_within_Cks1.pdb out.pdb')
-runCommand('findclash #1:5,6,7,8 test #0')
+runCommand('findclash #1:TOREPLACE test #0')
 
--- 9ClashAnalysis/driver1.sh
+++ 9ClashAnalysis/driver1.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 echo "Make sure to change atoms in chimScript.py"
 echo "Sequence Length: "
+replace ="5"
 read seqLength
+for (( i = 0; i < $seqLength+1; i++ )); do
+   if [[ $i > 5 ]]; then
+       replace+=",$i"
+   fi
+done
+sed -i "s.TOREPLACE.$replace.g" chimScript.py
 for i in {1..2}
 do
 	for j in {1..5}
 	do
 		mkdir -p s${i}/neutral${j}
 		cd s${i}/neutral${j}
-		cp ${dir}/s${i}/bemeta/prod$(($((seqLength*2))+$((j-1)))).xtc all.xtc
-		cp ${dir}/s${i}/bemeta/start0.tpr .
+        cp ../../../1trimTrajectory/s1/prod$(($((seqLength*2))+$((j-1)))).xtc all_pbc.xtc
+        cp ../../../../s1${i}/start0.tpr .
 		cp ../../submit1.job .
 		cp ../../*.py .
 		cp ../../*pdb .
