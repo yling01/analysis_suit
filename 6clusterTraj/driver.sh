7349b90a6065a7fb26113d2baf652251a18164a3
Tim Ling <43307135+yling01@users.noreply.github.com>
2020-07-13 13:44:02 -0400

    Update driver.sh

--- 6clusterTraj/driver.sh
+++ 6clusterTraj/driver.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
+module load python/3.6.0
+module load gcc/7.3.0
+module load cuda/10.2
+module load openmpi/2.1.2
+module load libmatheval
+export PATH
+source /cluster/tufts/ysl8/jovan/gromacs_linlab_avx512/bin/GMXRC.bash
+export PLUMED_KERNEL=/cluster/tufts/ysl8/jovan/gromacs_linlab_avx2/plumed/lib/libplumedKernel.so
+export GMXLIB=/cluster/tufts/ylin12/tim/localGMXLIB
 for i in {1..2}
 do
     cd s${i}
     ./Sh_make_cluster_xtc.sh
     cd ../
 done
-
