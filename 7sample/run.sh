6034689a045c47efd5cce8944e8eee9af15a9397
Tim Ling <43307135+yling01@users.noreply.github.com>
2020-07-13 11:49:26 -0400

    Update run.sh

--- 7sample/run.sh
+++ 7sample/run.sh
@@ -21,8 +21,6 @@ for i in {1..2}
 do
 	for j in {1..5}
 	do
-		#echo 4 4 | gmx_mpi rms -s ${file} -f s${i}cluster${j}/100random.pdb -nomw -o s${i}cluster${j}/rmsd.xvg
-		#python calcStat.py s${i}cluster${j}/rmsd.xvg s${i}cluster${j}/stat.txt
         	python calculateRMSD.py --ref ${file} --output s${i}cluster${j}/stat.txt --trajectory s${i}cluster${j}/100random.pdb
 		cd s${i}cluster${j}
 		vmd -m 100random.pdb ../Cks1.pdb < ../script.vmd
