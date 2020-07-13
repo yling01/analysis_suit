6f5b46c806c7438d9c27baed7ee8d14a025d5b37
Tim Ling <43307135+yling01@users.noreply.github.com>
2020-07-12 23:48:39 -0400

    Update Py_searchForCis.py

--- 1trimTrajectory/Py_searchForCis.py
+++ 1trimTrajectory/Py_searchForCis.py
@@ -10,9 +10,10 @@ currFile = open("struct_omega.xvg", "r")
 for line in currFile:
     words = line.split()
     if words[0] != "#" and words[0] != "@":
+	frame = words[0]
         for index, word in enumerate(words):
-            if index > 1 and abs(float(word)) < 150:
-                print("Cis bond found!")
+            if index > 1 and abs(float(word)) < 90:
+                print("Cis bond found at frame %d at %d omega bond" % (frame, index)
                 cisFound = True
 if not cisFound:
 	print("No cis angles found")
