61fa410c6d15fae22c3c6590630e9d11dad0c23a
Tim Ling <43307135+yling01@users.noreply.github.com>
2020-07-13 11:48:21 -0400

    Delete calcStat.py

--- 7sample/calcStat.py
+++
@@ -1,8 +0,0 @@
-import sys
-import numpy as np
-import matplotlib.pyplot as plt
-fileName = sys.argv[2]
-RMSD = np.loadtxt(sys.argv[1], usecols = (1,), comments = ["#", "@"])
-print(fileName)
-with open(fileName, "w+") as fo:
-    fo.write("%.3f\n%.3f" % ((10 * np.mean(RMSD)), (10* np.std(RMSD))))
