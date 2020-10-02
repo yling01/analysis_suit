1e5497da741fcc8a555bb869a9b1e5869505898f
Yingjie Ling <yingjie.ling@tufts.edu>
2020-10-02 15:25:25 -0400

    Added density plots

--- 5clusterAnalysis/dPCA.py
+++ 5clusterAnalysis/dPCA.py
@@ -86,6 +86,13 @@ h1, h2 = create_histogram(projection[:trajectory_len], projection[trajectory_len
 s1_density, s1_density_clean = combine_density_coor(h1, 0.1000)
 s2_density, s2_density_clean = combine_density_coor(h2, 0.1000)
 
+np.savetxt(s1_dir + "density.txt", density_coor_array, fmt = "%10.5f")
+os.system("gnuplot -e \"TITLE=\'%s\'; INPUT=\'%s/density.txt\'\" plot.gplt" % (s1_dir, file_prefix))
+os.system("convert -density 300 %s/tmp.eps %s/density.png" % (s1_dir, s1_dir))
+np.savetxt(s2_dir + "density.txt", density_coor_array, fmt = "%10.5f")
+os.system("gnuplot -e \"TITLE=\'%s\'; INPUT=\'%s/density.txt\'\" plot.gplt" % (s2_dir, file_prefix))
+os.system("convert -density 300 %s/tmp.eps %s/density.png" % (s2_dir, s2_dir))
+
 print("Performing cluster analysis...")
 projection_cluster_assignment1 = get_cluster_assignment(s1_density_clean, projection[:trajectory_len], "s1_decision_graph.png", interactive, s1_dir)
 projection_cluster_assignment2 = get_cluster_assignment(s2_density_clean, projection[trajectory_len:], "s2_decision_graph.png", interactive, s2_dir)
