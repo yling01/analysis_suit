b650d73595bf6aab6a0cabf33aa909fb5a58e768
Yingjie Ling <yingjie.ling@tufts.edu>
2020-10-26 16:45:01 -0400

    Sequence with different font size. Range of probability and rmsd peak.

--- 10graph/graph.py
+++ 10graph/graph.py
@@ -83,27 +83,83 @@ def setTickes(axis, xmax, ymax):
     ticky = axis.yaxis.get_major_ticks()
 
     for tick in tickx:
-        tick.label.set_fontsize(15)
+        tick.label.set_fontsize(14)
     for tick in ticky:
-        tick.label.set_fontsize(15)
+        tick.label.set_fontsize(14)
 
-def properWrapper(axis, title, xmax, ymax, index_curr_row, index_curr_col, total, sequence):
-    # axis.legend(loc=1, prop={'family': 'monospace', 'size': 70})
-    # if index_curr_row == 0:
-    #     title = title + "\n\n" + sequence
-    # else:
-    title = sequence
+def properWrapper(axis, sequence, probabilityCutoff, probability, peak_x):
+    ax_seq = axis[0]
+    ax_descrition = axis[1]
+    description = "Pr(RMSD < %.1fÅ) = %.2f%% ± %.2f%%\n\
+    Peaks At %.3fÅ ± %.3fÅ\n" % (probabilityCutoff,
+                                                     probability[0],
+                                                     probability[1],
+                                                     peak_x[0],
+                                                     peak_x[1])
+    ax_descrition.text(0.5, 0.02, description, fontsize=14, va='bottom', ha='center')
+    ax_seq.text(0.5, 0.25, sequence, fontsize=20, va='bottom', ha='center')
 
-    axis.set_title(title, fontsize=15)
+def calculateError(s1_metric, s2_metric):
+    average = 0.5 * (s1_metric + s2_metric)
+    errorBar = s1_metric - average if s1_metric > average else s2_metric - average
+    return (average, errorBar)
 
-    setTickes(axis, xmax, ymax)
+def makeBlankFigure(NPX, NPY):
 
-    if index_curr_row == total - 1:
-        axis.set_xlabel("RMSD (Å)", fontsize=15)
+    fig = plt.figure(figsize = (8.25, 6 * NPY), dpi = 300)
 
-    if index_curr_col == 0:
-        axis.set_ylabel("Probability Density", fontsize=15)
+    left, bot, right, top = (0.1, 0.1, 0.90, 0.90)
+    HSpace = 0.4 * (top - bot) / NPY
+    WSpace = 0.4 * (right - left) / NPX
 
+    SubPlotH = (top - bot - (NPY - 1) * HSpace) / NPY
+    SubPlotW = (right - left - (NPX - 1) * WSpace) / NPX
+
+    plot_axes = []
+    text_axes = []
+
+
+    for y in range(NPY):
+        plot_axes_temp = []
+        text_axes_temp = []
+        for x in range(NPX):
+            plot_l = left + x * (SubPlotW + WSpace)
+            plot_b = bot + y * (SubPlotH + HSpace)
+
+            plot_ax = fig.add_axes([plot_l, plot_b, SubPlotW, SubPlotH])
+            text_ax_seq = fig.add_axes([plot_l, plot_b + SubPlotH * 1.2, SubPlotW, 0.1])
+            text_ax_description = fig.add_axes([plot_l, plot_b + SubPlotH, SubPlotW, 0.1])
+
+            if x == 0:
+                plot_ax.set_ylabel("Probability Density", fontsize=14)
+            if y == 0:
+                plot_ax.set_xlabel("RMSD (Å)", fontsize=14)
+            text_ax_seq.axis('off')
+            text_ax_description.axis('off')
+
+            text_axes_temp.append((text_ax_seq, text_ax_description))
+            plot_axes_temp.append(plot_ax)
+
+        plot_axes.append(plot_axes_temp)
+        text_axes.append(text_axes_temp)
+
+    title_text_all_l = left
+    title_text_all_b = bot + NPY * (SubPlotH + HSpace)
+    title_text_all_ax = fig.add_axes([title_text_all_l,
+                                  title_text_all_b,
+                                  SubPlotW, 0.1])
+    title_text_all_ax.text(0.5, 0, "All Frames", fontsize=24, va='bottom', ha='center', transform=title_text_all_ax.transAxes)
+    title_text_all_ax.axis('off')
+
+    title_text_all_l = left + (SubPlotW + WSpace)
+    title_text_all_b = bot + NPY * (SubPlotH + HSpace)
+    title_text_all_ax = fig.add_axes([title_text_all_l,
+                                  title_text_all_b,
+                                  SubPlotW, 0.1])
+    title_text_all_ax.text(0.5, 0, "Frames with No Clash", fontsize=24, va='bottom', ha='center', transform=title_text_all_ax.transAxes)
+    title_text_all_ax.axis('off')
+
+    return fig, plot_axes, text_axes
 
 if __name__ == "__main__":
     parser = optparse.OptionParser()
@@ -131,8 +187,8 @@ if __name__ == "__main__":
 
     color = ['#e8173a', '#22dd34', '#260bf4', '#335cff', '#0a6902', '#0fa103', '#ae016c', '#fc019c','#be0a35', '#f76d8e']
     assert len(seq) <= len(color)
-    fig, axs_all = plt.subplots(nrows=len(seq), ncols=2, sharex=False, sharey=False, figsize=(10, 8 * len(seq)))
-    fig.subplots_adjust(hspace=0.5, wspace=0.8)
+
+    fig, plot_axes, text_axes = makeBlankFigure(2, len(seq))
 
     vf = np.vectorize((lambda x : 10 * x))
     ymax = 0.0
@@ -140,8 +196,6 @@ if __name__ == "__main__":
     probability = []
     peak_x = []
     for index in range(len(seq)):
-
-
         rmsdHotLoop_mtx1 = np.loadtxt(RMSDFile_s1[index], comments=["#", "@"], usecols=1)
         clash_mtx1 = np.loadtxt(clashFile_s1[index], usecols=0, dtype=str)
 
@@ -159,8 +213,8 @@ if __name__ == "__main__":
 
         XTicksHotLoop = np.linspace(0, max(np.amax(rmsdHotLoop_mtx1), np.amax(rmsdHotLoop_mtx2)), binSizeHotLoop)
 
-        HotLoopX, HotLoopAllY1, HotLoopNoClashY1, s1_all_prob, s1_noC_prob = drawAllAndNoClash(axs_all[index][0],
-                                                                                               axs_all[index][1],
+        HotLoopX, HotLoopAllY1, HotLoopNoClashY1, s1_all_prob, s1_noC_prob = drawAllAndNoClash(plot_axes[index][0],
+                                                                                               plot_axes[index][1],
                                                                                                rmsdHotLoop_mtx1,
                                                                                                clash_mtx1,
                                                                                                seq[index],
@@ -168,8 +222,8 @@ if __name__ == "__main__":
                                                                                                XTicksHotLoop,
                                                                                                probabilityCutoff)
 
-        HotLoopX, HotLoopAllY2, HotLoopNoClashY2, s2_all_prob, s2_noC_prob = drawAllAndNoClash(axs_all[index][0],
-                                                                                               axs_all[index][1],
+        HotLoopX, HotLoopAllY2, HotLoopNoClashY2, s2_all_prob, s2_noC_prob = drawAllAndNoClash(plot_axes[index][0],
+                                                                                               plot_axes[index][1],
                                                                                                rmsdHotLoop_mtx2,
                                                                                                clash_mtx2,
                                                                                                "",
@@ -180,18 +234,22 @@ if __name__ == "__main__":
         HotLoopAll_max_x1 = HotLoopX[np.argmax(HotLoopAllY1)]
         HotLoopAll_max_x2 = HotLoopX[np.argmax(HotLoopAllY2)]
 
-        # axs_all[index][0].axvline(HotLoopAll_max_x1, ls="--")
-        # axs_all[index][0].axvline(HotLoopAll_max_x2, ls="--")
+        # plot_axes[index][0].axvline(HotLoopAll_max_x1, ls="--")
+        # plot_axes[index][0].axvline(HotLoopAll_max_x2, ls="--")
         HotLoopNoClash_max_x1 = HotLoopX[np.argmax(HotLoopNoClashY1)]
         HotLoopNoClash_max_x2 = HotLoopX[np.argmax(HotLoopNoClashY2)]
-        # axs_all[index][1].axvline(HotLoopNoClash_max_x1, ls="--")
-        # axs_all[index][1].axvline(HotLoopNoClash_max_x2, ls="--")
-        all_peak_x = 0.5 * (HotLoopAll_max_x1 + HotLoopAll_max_x2)
-        noC_peak_x = 0.5 * (HotLoopNoClash_max_x1 + HotLoopNoClash_max_x2)
+        # plot_axes[index][1].axvline(HotLoopNoClash_max_x1, ls="--")
+        # plot_axes[index][1].axvline(HotLoopNoClash_max_x2, ls="--")
+
+
+        all_peak_x = calculateError(HotLoopAll_max_x1, HotLoopAll_max_x2)
+        noC_peak_x = calculateError(HotLoopNoClash_max_x1, HotLoopNoClash_max_x2)
+
         peak_x.append((all_peak_x, noC_peak_x))
 
-        all_prob = 0.5 * (s1_all_prob + s2_all_prob)
-        noC_prob = 0.5 * (s1_noC_prob + s2_noC_prob)
+        all_prob = calculateError(s1_all_prob, s2_all_prob)
+        noC_prob = calculateError(s1_noC_prob, s2_noC_prob)
+
 
         probability.append((all_prob, noC_prob))
 
@@ -199,15 +257,19 @@ if __name__ == "__main__":
         if ymax < curr_max:
             ymax = curr_max
 
-        drawShadow(axs_all[index][1], HotLoopX, HotLoopNoClashY1, HotLoopNoClashY2, color[index])
-        drawShadow(axs_all[index][0], HotLoopX, HotLoopAllY1, HotLoopAllY2, color[index])
+        drawShadow(plot_axes[index][1], HotLoopX, HotLoopNoClashY1, HotLoopNoClashY2, color[index])
+        drawShadow(plot_axes[index][0], HotLoopX, HotLoopAllY1, HotLoopAllY2, color[index])
 
     ymax += 1.0
-    
-    for index in range(len(seq)):
-        #properWrapper(axs_all[index][1], "No Clash Only", RMSDCutoff, 6.0, index, 1, len(seq), "%s\nPr(RMSD < %.1fÅ | No Clash) = %.2f%%\nHighest Probability Found At %.3fÅ\n" % (seq[index], probabilityCutoff, probability[index][1], peak_x[index][1]))
-        #properWrapper(axs_all[index][0], "All Frames", RMSDCutoff, 6.0, index, 0, len(seq), "%s\nPr(RMSD < %.1fÅ) = %.2f%%\nHighest Probability Found At %.3fÅ\n" % (seq[index], probabilityCutoff, probability[index][0], peak_x[index][0]))
-        properWrapper(axs_all[index][1], "No Clash Only", RMSDCutoff, 6.0, index, 1, len(seq), "Pr(RMSD < %.1fÅ | No Clash) = %.2f%%\nHighest Probability Found At %.3fÅ\n" % (probabilityCutoff, probability[index][1], peak_x[index][1]))
-        properWrapper(axs_all[index][0], "All Frames", RMSDCutoff, 6.0, index, 0, len(seq), "Pr(RMSD < %.1fÅ) = %.2f%%\nHighest Probability Found At %.3fÅ\n" % (probabilityCutoff, probability[index][0], peak_x[index][0]))
 
-    fig.savefig(filename="histogram.png", dpi=300)
+    for index in range(len(seq)):
+        for i in range(2):
+            properWrapper(text_axes[index][i],
+                          seq[index],
+                          probabilityCutoff,
+                          probability[index][i],
+                          peak_x[index][i])
+            setTickes(plot_axes[index][i], 2.0, ymax)
+
+
+    fig.savefig(fname="histogram.png", dpi=300, bbox_inches='tight')
